5a9b81833330995094febb50f4baaa925a17d9b2
cvevera <cvevera@gmail.com>
2019-12-01 23:14:57 -0500

    added code that changes the database stock quanity on user imput

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,12 +14,13 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 
-    start();
+    displayProducts();
+    productPurchase();
 });
 
 // inquire function ID question then units question which triggers two functions
 
-function start() {
+function displayProducts() {
     connection.query("SELECT * FROM product", function (err, results) {
         if (err) throw err;
         console.log("Product Inventory")
@@ -31,34 +32,45 @@ function start() {
                 "Department: " + results[i].department_name + " |",
                 "Price: " + results[i].price + " |",
                 "Quantity: " + results[i].stock_quanity);
-        };
-        inquirer
-            .prompt([
-                {
-                    name: "idprompt",
-                    type: "input",
-                    message: "Please type the id number of the product you would like to purchase.",
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
-                    }
-                },
-                {
-                    name: "quanityprompt",
-                    type: "input",
-                    message: "How many units would you like to purchase?",
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
-                    }
-                }
-            ])
-        // .then(function (answer) {
-
-        // });
+        }
     })
 };
+function productPurchase() {
+    inquirer
+        .prompt([
+            {
+                name: "idprompt",
+                type: "input",
+                message: "Please type the id number of the product you would like to purchase.",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "quanityprompt",
+                type: "input",
+                message: "How many units would you like to purchase?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function (answer) {
+            var query = "SELECT * FROM product WHERE ?";
+            connection.query(query, { item_id: answer.idprompt }, function (err, results) {
+                // console.log(results[0].stock_quanity)
+                if (err) throw err;
+                var queryUpdate = 'UPDATE product SET stock_quanity = ' + (results[0].stock_quanity - answer.quanityprompt) + ' WHERE item_id = ' + answer.idprompt;
+                connection.query(queryUpdate, function (err, res) {
+                    if (err) throw err
+                displayProducts();
+
+                    })
+            })
+        })};

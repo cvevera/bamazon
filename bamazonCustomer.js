860676a33122aac3ff1afcfc4150ab6df476d992
cvevera <cvevera@gmail.com>
2019-12-01 22:10:39 -0500

    updated display products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,35 +20,45 @@ connection.connect(function (err) {
 // inquire function ID question then units question which triggers two functions
 
 function start() {
-    connection.query("SELECT * FROM product", function(err, results) {
+    connection.query("SELECT * FROM product", function (err, results) {
         if (err) throw err;
-        console.table(results);
-    inquirer
-        .prompt([
-            {
-                name: "idprompt",
-                type: "number",
-                message: "Please type the id number of the product you would like to purchase.",
-                validate: function(value) {
-                    if (isNaN(value) === false) {
-                      return true;
+        console.log("Product Inventory")
+        console.log("-----------------------------")
+        for (var i = 0; i < results.length; i++) {
+            console.log(
+                "Id: " + results[i].item_id + " |",
+                "Product: " + results[i].product_name + " |",
+                "Department: " + results[i].department_name + " |",
+                "Price: " + results[i].price + " |",
+                "Quantity: " + results[i].stock_quanity);
+        };
+        inquirer
+            .prompt([
+                {
+                    name: "idprompt",
+                    type: "input",
+                    message: "Please type the id number of the product you would like to purchase.",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
                     }
-                    return false;
-                  }
-            },
-            {
-                name: "quanityprompt",
-                type: "number",
-                message: "How many units would you like to purchase?",
-                validate: function(value) {
-                    if (isNaN(value) === false) {
-                      return true;
+                },
+                {
+                    name: "quanityprompt",
+                    type: "input",
+                    message: "How many units would you like to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
                     }
-                    return false;
-                  }
-            }
-        ])
+                }
+            ])
         // .then(function (answer) {
 
         // });
-})};
+    })
+};
